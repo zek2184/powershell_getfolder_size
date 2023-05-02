5006190f8c20643e63991585da204e3f99043ff4
Ezekeil Pahila <ezekeilvpahila@gmail.com>
2023-05-01 20:58:22 -0400

    Updating comments to get folder and size.

--- folderSize.ps1
+++ folderSize.ps1
@@ -1,11 +1,12 @@
 # Created by: Ezekeil Pahila
 # Date: 05012023
 # Descriotion: PowerShell will return folder name and size per folder.
+# Referrence: https://www.spguides.com/check-file-size-using-powershell/
 
-# Get folder size 
+# Get folder location and expand Name 
 $sourceFolder = gci .\ | Select-Object -ExpandProperty Name
 
-# iterate eacg folder and display size
+# iterate each folder and display size
 foreach ($f in $sourceFolder) { 
     $foldersize = Get-ChildItem $f -recurse | Measure-Object -property length -sum
     $foldersize = $foldersize.sum / 1GB
